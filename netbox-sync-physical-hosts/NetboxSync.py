0d8a0536ef9905a1ccefbb2827256ba29bbe469c
guanana <guanana2@gmail.com>
2021-01-06 02:36:05 +0000

    Split config into it's own module Add more tests

---
+++ netbox-sync-physical-hosts/NetboxSync.py
@@ -0,0 +1,30 @@
+import sys
+from config.config import parse_config
+from netboxhandler.NetBoxHandler import NetBoxHandler
+from modules.NmapHandler import NmapServiceScan, NmapMacScan, NmapBasic
+
+
+def main(conf):
+    nb = NetBoxHandler(conf.nb_url, conf.nb_token,
+                       conf.nb_ignore_tls_errors, conf.tag, conf.clenaup)
+
+    if conf.get_mac:
+        nmap = NmapMacScan(conf.networks)
+        hosts = nmap.run()
+        nb.run(hosts)
+
+    if conf.get_services:
+        nmap = NmapServiceScan(conf.networks)
+        hosts = nmap.run()
+        nb.run(hosts)
+
+    if not conf.get_mac and not conf.get_services:
+        nmap = NmapBasic(conf.networks)
+        hosts = nmap.run()
+        nb.run(hosts)
+
+
+if __name__ == '__main__':
+    conf = parse_config()
+    sys.exit(main(conf))
+
---
+++ netbox-sync-physical-hosts/config/tests/test_config.py
@@ -0,0 +1,20 @@
+from unittest.mock import MagicMock
+import pytest
+
+from config import parse_config
+
+def test_get_conf_call(monkeypatch):
+    testargs = ["prog", "-u", "http://test", "-p", "1234", "-n", "127.0.0.1"]
+    monkeypatch.setattr('sys.argv', testargs)
+    testconf = parse_config()
+    assert testconf.nb_url == "http://test"
+    assert testconf.nb_token == "1234"
+    assert testconf.networks == "127.0.0.1"
+
+def test_failed_conf_call(monkeypatch):
+    with pytest.raises(SystemExit) as pytest_wrapped_e:
+        testargs = ["prog", "-c", "test"]
+        monkeypatch.setattr('sys.argv', testargs)
+        parse_config()
+        assert pytest_wrapped_e.value.code == 2
+
